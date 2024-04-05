3ba90b21981512b295b97eae4b969d9c3bc76e49
skuhlmann <samkuhlmann@gmail.com>
2024-04-05 09:57:54 -0600

    starts data fetch

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,42 @@
+import "dotenv/config";
+
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Frog, TextInput, Button } from "frog";
-import { Box, Heading, vars } from "./ui.js";
+import { Box, Heading, Text, vars } from "./ui.js";
 import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
+import { formatEther } from "viem";
+import { formatShortDateTimeFromSeconds, postData } from "./helpers.js";
+
+const GRAPH_ENDPOINT = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.GRAPH_KEY}/subgraphs/id/6vyAqRpCyrhLsfd6TfYAssvKywKhxJykkDbPxJZ4ZcEr`;
 
 export const app = new Frog({
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
   ui: { vars },
-}).frame("/", (c) => {
+}).frame("/", async (c) => {
+  //todo: why won't this work with given a path, need to pass a yeeter shaman address in the url
+  // const yeeterid = c.req.param("yeeterid");
+  const yeeterid = "0x7a023e66b1f607ee9ab3f384198adf668847a4d9";
+
+  const yeetData = await postData(GRAPH_ENDPOINT, {
+    query: `{yeeter(id: "${yeeterid}") {id endTime startTime minTribute multiplier goal balance}}`,
+  });
+
+  //todo: handle errors - if not yeeter display erorr so tx can't go forward
+  console.log("yeetData", yeetData);
+
+  const balance = formatEther(yeetData.data.yeeter.balance);
+  const endTime = formatShortDateTimeFromSeconds(yeetData.data.yeeter.endTime);
+  const startTime = formatShortDateTimeFromSeconds(
+    yeetData.data.yeeter.startTime
+  );
+  const goal = formatEther(yeetData.data.yeeter.goal);
+
+  //todo: pass this to the tx frame to popluate the value
+  const minTribute = formatEther(yeetData.data.yeeter.minTribute);
+  // const balance = "0";
+
   // const { inputText } = c;
   // const amount = inputText;
   return c.res({
@@ -21,17 +49,25 @@ export const app = new Frog({
         padding="32"
       >
         <Heading size="64">YEET</Heading>
+        <Text>
+          Raised {balance} of the {goal} goal
+        </Text>
+        <Text>
+          Starts {startTime} and ends {endTime}
+        </Text>
+        <Text>Yeet amount: {minTribute} base eth</Text>
       </Box>
     ),
     intents: [
       <TextInput placeholder="Amount of ETH" />,
-      <Button.Transaction target="/yeet">YEET</Button.Transaction>,
+      <Button.Transaction target={`yeet/${yeeterid}`}>YEET</Button.Transaction>,
     ],
   });
 });
 
-app.transaction("/yeet", (c) => {
+app.transaction("/yeet/:yeeterid", (c) => {
   // Contract transaction response.
+  const yeeterid = c.req.param("yeeterid");
   return c.contract({
     abi: [
       {
@@ -47,7 +83,7 @@ app.transaction("/yeet", (c) => {
     functionName: "contributeEth",
     value: 10000000000000000n,
     args: ["1st Yeet from a frame"],
-    to: "0x8bd3eca1b83188f46171e6ef104c80fc82ceb861",
+    to: yeeterid as `0x${string}`,
   });
 });
 
