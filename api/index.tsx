05d60d6f60999c0576fedc40c24483eb9305a30b
earth2travis <travis.mccutcheon@gmail.com>
2024-04-17 21:44:45 -0500

    Comment out devtools

--- api/index.tsx
+++ api/index.tsx
@@ -678,9 +678,7 @@ app.transaction('/yeet/:yeeterid/:mintribute', c => {
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined';
 const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development';
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
-
-console.log(process.version);
+// devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
 
 export const GET = handle(app);
 export const POST = handle(app);
