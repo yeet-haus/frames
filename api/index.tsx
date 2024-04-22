51e0345bdee760ddc7afe076255da268f82a7893
earth2travis <travis.mccutcheon@gmail.com>
2024-04-21 22:45:52 -0500

    Update packages and change extension

--- api/index.tsx
+++ api/index.tsx
@@ -678,7 +678,7 @@ app.transaction('/yeet/:yeeterid/:mintribute', c => {
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined';
 const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development';
-// devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic });
 
 export const GET = handle(app);
 export const POST = handle(app);
